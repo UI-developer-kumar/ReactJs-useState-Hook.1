afd1ca808b6a57a2d259bb27d9a9ffec571b7180
UI-developer-kumar <premkumar9372@gmail.com>
2024-07-08 16:29:31 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import Effect from "./1-useEffect";
+// import Effect from "./1-useEffect";
+import SearchField from "./2-useEffect-example-2";
 
 const  App = () => {
   return (
     <div className="container">
-      <Effect />
+      {/* <Effect /> */}
+      <SearchField />
     </div>
   );
 }
